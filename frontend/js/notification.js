7a5b7033a733bde30f7a0d004b49f241aea34d46
Georg Sieber <it@georg-sieber.de>
2021-06-28 22:19:25 +0200

    set notification tag to avoid multiple notification for the same event when multiple tabs are open

--- frontend/js/notification.js
+++ frontend/js/notification.js
@@ -41,12 +41,11 @@ function checkNotification(newNotificationInfo) {
 	if(notificationInfo != null) {
 		newNotificationInfo['job_container'].forEach(function(item1) {
 			notificationInfo['job_container'].forEach(function(item2) {
-				if(item1.id == item2.id) {
-					if(item1.state != item2.state) {
-						notify(item1.name+': '+item1.state_description, L__JOB_CONTAINER_STATUS_CHANGED, 'img/job.dyn.svg',
-							'index.php?explorer-content='+encodeURIComponent('views/job-container.php?id='+item1.id)
-						);
-					}
+				if(item1.id == item2.id && item1.state != item2.state) {
+					notify(item1.name+': '+item1.state_description, L__JOB_CONTAINER_STATUS_CHANGED, 'img/job.dyn.svg',
+						'index.php?explorer-content='+encodeURIComponent('views/job-container.php?id='+item1.id),
+						'job#'+item1.id+'#'+item1.state
+					);
 				}
 			});
 		});
@@ -54,10 +53,11 @@ function checkNotification(newNotificationInfo) {
 	notificationInfo = newNotificationInfo;
 }
 
-function notify(title, body, icon, link) {
+function notify(title, body, icon, link, tag) {
 	if(Notification.permission !== 'granted') return;
+	console.log(title);
 	var notification = new Notification(title, {
-		icon: icon, body: body,
+		icon: icon, body: body, tag: tag
 	});
 	notification.onclick = function() {
 		window.open(link);
