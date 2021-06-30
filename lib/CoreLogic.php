0f4e8c8127b2ca77ab26dbefef4f253ab439f5a4
Georg Sieber <it@georg-sieber.de>
2021-06-30 13:03:21 +0200

    reduce database queries

--- lib/CoreLogic.php
+++ lib/CoreLogic.php
@@ -274,8 +274,9 @@ class CoreLogic {
 				foreach($packages as $pid => $package) {
 
 					// check OS compatibility
-					if(!empty($package['compatible_os']) && !empty($this->db->getComputer($computer_id)->os)
-					&& $package['compatible_os'] != $this->db->getComputer($computer_id)->os) {
+					$tmpComputer = $this->db->getComputer($computer_id);
+					if(!empty($package['compatible_os']) && !empty($tmpComputer->os)
+					&& $package['compatible_os'] != $tmpComputer->os) {
 						// create failed job
 						if($this->db->addJob($jcid, $computer_id,
 							$pid, $package['procedure'], $package['success_return_codes'],
@@ -288,8 +289,8 @@ class CoreLogic {
 						}
 						continue;
 					}
-					if(!empty($package['compatible_os_version']) && !empty($this->db->getComputer($computer_id)->os_version)
-					&& $package['compatible_os_version'] != $this->db->getComputer($computer_id)->os_version) {
+					if(!empty($package['compatible_os_version']) && !empty($tmpComputer->os_version)
+					&& $package['compatible_os_version'] != $tmpComputer->os_version) {
 						// create failed job
 						if($this->db->addJob($jcid, $computer_id,
 							$pid, $package['procedure'], $package['success_return_codes'],
