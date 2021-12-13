676cfe50eaf596d42137aae7c3a17952c4c097d8
Georg Sieber <it@georg-sieber.de>
2021-12-13 21:25:38 +0100

    check user account on each web request

--- frontend/session.php
+++ frontend/session.php
@@ -2,15 +2,21 @@
 
 session_start();
 
+// check if user has a valid session
 if(!isset($_SESSION['um_username'])) {
 	redirectToLogin();
 }
+// check if user account still exists and is not locked
+$systemUser = $db->getSystemuser($_SESSION['um_userid']);
+if($systemUser === null || !empty($systemUser->locked)) {
+	redirectToLogin(true);
+}
 
-function redirectToLogin() {
+function redirectToLogin($forceLogout=false) {
 	global $SUBVIEW;
 	header('HTTP/1.1 401 Not Authorized');
 	if(empty($SUBVIEW)) {
-		header('Location: login.php');
+		header('Location: login.php'.($forceLogout ? '?logout=1' : ''));
 	}
 	die();
 }
