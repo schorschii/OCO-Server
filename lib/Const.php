dbf9b56f55f89b977be8d6537e2b4efb7a6f1894
Georg Sieber <it@georg-sieber.de>
2021-12-14 16:45:52 +0100

    bump version

--- lib/Const.php
+++ lib/Const.php
@@ -1,6 +1,6 @@
 <?php
 
-const APP_VERSION = '0.12.0';
+const APP_VERSION = '0.12.1';
 const APP_RELEASE = 'Christmas Release 🛷';
 
 const UPDATE_API_URL = 'https://api.github.com/repos/schorschii/oco-server/releases';
