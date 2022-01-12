701a24676cfa46586fa2d139391c373fd7ae2060
Georg Sieber <it@georg-sieber.de>
2022-01-12 23:58:38 +0100

    fix JS errors

--- frontend/views/packages.php
+++ frontend/views/packages.php
@@ -28,23 +28,23 @@ try {
 <?php if($group !== null) { ?>
 	<h1><img src='img/folder.dyn.svg'><span id='page-title'><?php echo htmlspecialchars($db->getPackageGroupBreadcrumbString($group->id)); ?></span><span id='spnPackageGroupName' class='rawvalue'><?php echo htmlspecialchars($group->name); ?></span></h1>
 	<div class='controls'><span><?php echo LANG['group']; ?>:&nbsp;</span>
-		<button onclick='createPackageGroup(<?php echo $group->id; ?>)' <?php if(!$currentSystemUser->checkPermission($group, PermissionManager::METHOD_CREATE, false)) echo 'disabled'; ?>><img src='img/folder-new.svg'>&nbsp;<?php echo LANG['new_subgroup']; ?></button> 
-		<button onclick='refreshContentDeploy([],[<?php echo $group->id; ?>])' <?php if(empty($packages) || !$currentSystemUser->checkPermission($packages[0], PermissionManager::METHOD_DEPLOY, false)) echo 'disabled'; ?>><img src='img/deploy.svg'>&nbsp;<?php echo LANG['deploy_all']; ?></button> 
-		<button onclick='renamePackageGroup(<?php echo $group->id; ?>, spnPackageGroupName.innerText)' <?php if(!$currentSystemUser->checkPermission($group, PermissionManager::METHOD_WRITE, false)) echo 'disabled'; ?>><img src='img/edit.svg'>&nbsp;<?php echo LANG['rename_group']; ?></button> 
-		<button onclick='confirmRemovePackageGroup([<?php echo $group->id; ?>], event, spnPackageGroupName.innerText)' <?php if(!$currentSystemUser->checkPermission($group, PermissionManager::METHOD_DELETE, false)) echo 'disabled'; ?>><img src='img/delete.svg'>&nbsp;<?php echo LANG['delete_group']; ?></button> 
+		<button onclick='createPackageGroup(<?php echo $group->id; ?>)' <?php if(!$currentSystemUser->checkPermission($group, PermissionManager::METHOD_CREATE, false)) echo 'disabled'; ?>><img src='img/folder-new.svg'>&nbsp;<?php echo LANG['new_subgroup']; ?></button>
+		<button onclick='refreshContentDeploy([],[<?php echo $group->id; ?>])' <?php if(empty($packages) || !$currentSystemUser->checkPermission($packages[0], PermissionManager::METHOD_DEPLOY, false)) echo 'disabled'; ?>><img src='img/deploy.svg'>&nbsp;<?php echo LANG['deploy_all']; ?></button>
+		<button onclick='renamePackageGroup(<?php echo $group->id; ?>, spnPackageGroupName.innerText)' <?php if(!$currentSystemUser->checkPermission($group, PermissionManager::METHOD_WRITE, false)) echo 'disabled'; ?>><img src='img/edit.svg'>&nbsp;<?php echo LANG['rename_group']; ?></button>
+		<button onclick='confirmRemovePackageGroup([<?php echo $group->id; ?>], event, spnPackageGroupName.innerText)' <?php if(!$currentSystemUser->checkPermission($group, PermissionManager::METHOD_DELETE, false)) echo 'disabled'; ?>><img src='img/delete.svg'>&nbsp;<?php echo LANG['delete_group']; ?></button>
 	</div>
 <?php } elseif($family !== null) { ?>
 	<h1><img src='<?php echo $family->getIcon(); ?>'><span id='page-title'><span id='spnPackageFamilyName'><?php echo htmlspecialchars($family->name); ?></span></span></h1>
 	<div class='controls'>
-		<button onclick='refreshContentPackageNew("<?php echo htmlspecialchars($family->name,ENT_QUOTES); ?>")' <?php if(!$currentSystemUser->checkPermission(new Package(), PermissionManager::METHOD_CREATE, false)) echo 'disabled'; ?>><img src='img/add.svg'>&nbsp;<?php echo LANG['new_version']; ?></button> 
+		<button onclick='refreshContentPackageNew("<?php echo htmlspecialchars($family->name,ENT_QUOTES); ?>")' <?php if(!$currentSystemUser->checkPermission(new Package(), PermissionManager::METHOD_CREATE, false)) echo 'disabled'; ?>><img src='img/add.svg'>&nbsp;<?php echo LANG['new_version']; ?></button>
 		<button onclick='renamePackageFamily(<?php echo $family->id; ?>, spnPackageFamilyName.innerText)' <?php if(!$currentSystemUser->checkPermission($family, PermissionManager::METHOD_WRITE, false)) echo 'disabled'; ?>><img src='img/edit.svg'>&nbsp;<?php echo LANG['rename']; ?></button>
 		<button onclick='editPackageFamilyNotes(<?php echo $family->id; ?>, spnPackageFamilyNotes.innerText)' <?php if(!$currentSystemUser->checkPermission($family, PermissionManager::METHOD_WRITE, false)) echo 'disabled'; ?>><img src='img/edit.svg'>&nbsp;<?php echo LANG['edit_description']; ?></button>
 		<button class='<?php echo (!empty($family->icon)?'nomarginright':''); ?>' onclick='fleIcon.click()' <?php if(!$currentSystemUser->checkPermission($family, PermissionManager::METHOD_WRITE, false)) echo 'disabled'; ?>><img src='img/image-add.svg'>&nbsp;<?php echo LANG['change_icon']; ?></button>
 		<?php if(!empty($family->icon)) { ?>
 			<button onclick='removePackageFamilyIcon(<?php echo $family->id; ?>)' <?php if(!$currentSystemUser->checkPermission($family, PermissionManager::METHOD_WRITE, false)) echo 'disabled'; ?>><img src='img/image-remove.svg'>&nbsp;<?php echo LANG['remove_icon']; ?></button>
 		<?php } ?>
-		<button onclick='currentExplorerContentUrl=\"views/package-families.php\";confirmRemovePackageFamily([<?php echo htmlspecialchars($family->id,ENT_QUOTES); ?>], spnPackageFamilyName.innerText)' <?php if(!$currentSystemUser->checkPermission($family, PermissionManager::METHOD_DELETE, false)) echo 'disabled'; ?>><img src='img/delete.svg'>&nbsp;<?php echo LANG['delete_package_family']; ?></button> 
-		<span class='fillwidth'></span> 
+		<button onclick='currentExplorerContentUrl="views/package-families.php";confirmRemovePackageFamily([<?php echo htmlspecialchars($family->id,ENT_QUOTES); ?>], spnPackageFamilyName.innerText)' <?php if(!$currentSystemUser->checkPermission($family, PermissionManager::METHOD_DELETE, false)) echo 'disabled'; ?>><img src='img/delete.svg'>&nbsp;<?php echo LANG['delete_package_family']; ?></button>
+		<span class='fillwidth'></span>
 		<span><a <?php echo explorerLink('views/package-families.php'); ?>><?php echo LANG['package_families']; ?></a></span>
 	</div>
 	<input type='file' id='fleIcon' style='display:none' onchange='editPackageFamilyIcon(<?php echo $family->id; ?>, this.files[0])'></input>
@@ -56,9 +56,9 @@ try {
 <?php } else { ?>
 	<h1><img src='img/package.dyn.svg'><span id='page-title'><?php echo LANG['complete_package_library']; ?></span></h1>
 	<div class='controls'>
-		<button onclick='refreshContentPackageNew()' <?php if(!$currentSystemUser->checkPermission(new Package(), PermissionManager::METHOD_CREATE, false)) echo 'disabled'; ?>><img src='img/add.svg'>&nbsp;<?php echo LANG['new_package']; ?></button> 
-		<button onclick='createPackageGroup()' <?php if(!$currentSystemUser->checkPermission(new PackageGroup(), PermissionManager::METHOD_CREATE, false)) echo 'disabled'; ?>><img src='img/folder-new.svg'>&nbsp;<?php echo LANG['new_group']; ?></button> 
-		<span class='fillwidth'></span> 
+		<button onclick='refreshContentPackageNew()' <?php if(!$currentSystemUser->checkPermission(new Package(), PermissionManager::METHOD_CREATE, false)) echo 'disabled'; ?>><img src='img/add.svg'>&nbsp;<?php echo LANG['new_package']; ?></button>
+		<button onclick='createPackageGroup()' <?php if(!$currentSystemUser->checkPermission(new PackageGroup(), PermissionManager::METHOD_CREATE, false)) echo 'disabled'; ?>><img src='img/folder-new.svg'>&nbsp;<?php echo LANG['new_group']; ?></button>
+		<span class='fillwidth'></span>
 		<span><a <?php echo explorerLink('views/package-families.php'); ?>><?php echo LANG['package_families']; ?></a></span>
 	</div>
 <?php } ?>
