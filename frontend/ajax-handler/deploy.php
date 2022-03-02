83bc735ec55026fa4b62c6e689c25e7812e55f10
Georg Sieber <it@georg-sieber.de>
2022-03-02 09:36:42 +0100

    do not select all per default

--- frontend/ajax-handler/deploy.php
+++ frontend/ajax-handler/deploy.php
@@ -23,7 +23,7 @@ try {
 			if(!$currentSystemUser->checkPermission($c, PermissionManager::METHOD_DEPLOY, false)) continue;
 
 			$selected = '';
-			if(!empty($group) || in_array($c->id, $select_computer_ids)) $selected = 'checked';
+			if(in_array($c->id, $select_computer_ids)) $selected = 'checked';
 			echo "<label class='block'><input type='checkbox' name='computers' onchange='refreshDeployCount()' value='".$c->id."' ".$selected." />".htmlspecialchars($c->hostname)."</label>";
 		}
 		die();
@@ -37,7 +37,7 @@ try {
 			if(!$currentSystemUser->checkPermission($p, PermissionManager::METHOD_DEPLOY, false)) continue;
 
 			$selected = '';
-			if(!empty($group) || in_array($p->id, $select_package_ids)) $selected = 'checked';
+			if(in_array($p->id, $select_package_ids)) $selected = 'checked';
 			echo "<label class='block'><input type='checkbox' name='packages' onchange='refreshDeployCount()' value='".$p->id."' ".$selected." />".htmlspecialchars($p->package_family_name)." (".htmlspecialchars($p->version).")"."</label>";
 		}
 		die();
