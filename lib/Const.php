e32bdaa672ba97920b49ec51d31bf769c7e26381
Georg Sieber <it@georg-sieber.de>
2022-07-21 15:30:08 +0200

    bump version

--- lib/Const.php
+++ lib/Const.php
@@ -1,6 +1,6 @@
 <?php
 
-const APP_VERSION = '0.13.8';
+const APP_VERSION = '0.14.0';
 const APP_RELEASE = '"Supertux"';
 
 const UPDATE_API_URL = 'https://api.github.com/repos/schorschii/oco-server/releases';
