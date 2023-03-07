538478fdbd9dc78b079aaa436cecce68fff16627
Georg Sieber <it@georg-sieber.de>
2023-03-07 12:06:35 +0100

    fix line breaks in JS

--- frontend/js/strings.js.php
+++ frontend/js/strings.js.php
@@ -4,6 +4,6 @@ header('Content-Type: text/javascript');
 
 echo "var LANG = {\n";
 foreach(LanguageController::getSingleton()->getMessages() as $key => $message) {
-    echo "\t'".$key."': '".str_replace("'",'"',str_replace("\n",'\\n',str_replace('\\','\\\\',$message)))."',\n";
+    echo "\t\"".$key."\": \"".str_replace('"',"'",str_replace("\n",'\\n',str_replace('\\','\\\\',$message)))."\",\n";
 }
 echo "}\n";
