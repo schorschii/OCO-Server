9df95a1994e6cf59889e7593159ca457af481639
Georg Sieber <it@georg-sieber.de>
2024-09-26 00:21:22 +0200

    fix parameter order

--- lib/Apple/VolumePurchaseProgram.class.php
+++ lib/Apple/VolumePurchaseProgram.class.php
@@ -68,7 +68,7 @@ class VolumePurchaseProgram {
 				echo 'Unable to get app metadata '.$asset['adamId']."\n";
 				continue;
 			}
-			$this->db->insertOrUpdateManagedApp($app['attributes']['name'], $asset['adamId'], $app['attributes']['platformAttributes']['ios']['bundleId'], $asset['totalCount']);
+			$this->db->insertOrUpdateManagedApp($app['attributes']['platformAttributes']['ios']['bundleId'], $asset['adamId'], $app['attributes']['name'], $asset['totalCount']);
 		}
 	}
 
