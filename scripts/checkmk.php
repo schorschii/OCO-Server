354169f7542bba6d9a7c9df50d36aafbe54616a5
Georg Sieber <it@georg-sieber.de>
2025-04-07 17:11:10 +0200

    add checkmk local check script

---
+++ scripts/checkmk.php
@@ -0,0 +1,29 @@
+#!/usr/bin/env php
+<?php
+if(php_sapi_name() != 'cli')
+	die('This script must be executed from command line.'."\n");
+
+require_once(__DIR__.'/../loader.inc.php');
+
+
+try {
+
+	// check license
+	$license = new LicenseCheck($db);
+	if(!$license->isValid()) {
+		echo '3 "OCO License" - "License is invalid!"' . "\n";
+	} elseif(!$license->isFree() && $license->getRemainingTime() < 60*60*24*14) {
+		$remainingDays = round($license->getRemainingTime() / (60*60*24));
+		echo '2 "OCO License" - "License expires in '.$remainingDays.' day(s)"' . "\n";
+	} else {
+		echo '0 "OCO License" - "License is valid until '.date('Y-m-d', $license->getExpireTime()).'"' . "\n";
+	}
+
+	// TODO: check last cron run
+
+} catch(Exception $e) {
+	echo $argv[1].' ERROR: '.$e->getMessage()."\n";
+	echo $e->getTraceAsString();
+	echo "\n";
+	exit(1);
+}
