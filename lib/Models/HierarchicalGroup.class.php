d7db2d938b7255dad21d434864f4b10fe3a243ef
Georg Sieber <it@georg-sieber.de>
2026-01-15 22:30:24 +0100

    simplify code

--- lib/Models/HierarchicalGroup.class.php
+++ lib/Models/HierarchicalGroup.class.php
@@ -23,16 +23,12 @@ abstract class HierarchicalGroup implements IHierarchicalGroup {
 	public function getBreadcrumbString() {
 		$currentGroupId = $this->getId();
 		$groupStrings = [];
-		while(true) {
+		while($currentGroupId) {
 			$currentGroup = call_user_func([$this->db, $this::GET_OBJECT_FUNCTION], $currentGroupId);
 			if(!$currentGroup instanceof IHierarchicalGroup)
 				throw new \Exception('Group object does not conform to IHierarchicalGroup');
 			$groupStrings[] = $currentGroup->getName();
-			if($currentGroup->getParentId() === null) {
-				break;
-			} else {
-				$currentGroupId = $currentGroup->getParentId();
-			}
+			$currentGroupId = $currentGroup->getParentId();
 		}
 		$groupStrings = array_reverse($groupStrings);
 		return implode(' » ', $groupStrings);
