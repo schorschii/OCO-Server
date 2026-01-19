aea3a8165003abee6e5736025baa8d0bab335417
Georg Sieber <it@georg-sieber.de>
2026-01-20 00:00:28 +0100

    Chrome macOS support

--- scripts/chrome-firefox-policies.php
+++ scripts/chrome-firefox-policies.php
@@ -9,26 +9,36 @@ $policyDefintions = $db->selectAllPolicyDefinition();
 foreach($policyDefintions as $pd) {
 	if(!$pd->manifestation_windows) continue;
 	$mWindowsSplit = explode(':', $pd->manifestation_windows);
-	if(count($mWindowsSplit) != 3) continue;
+	if(count($mWindowsSplit) < 2) continue;
 
-	if($mWindowsSplit[1] == 'Software\Policies\Google\Chrome') {
-		$mLinux = 'JSON:/etc/opt/chrome/policies/managed/default.json:'.$mWindowsSplit[2]."\n"
-			.'JSON:/etc/chromium/policies/managed/default.json:'.$mWindowsSplit[2];
-	} elseif($mWindowsSplit[1] == 'Software\Policies\Google\Chrome\Recommended') {
-		$mLinux = 'JSON:/etc/opt/chrome/policies/recommended/default.json:'.$mWindowsSplit[2]."\n"
-			.'JSON:/etc/chromium/policies/recommended/default.json:'.$mWindowsSplit[2];
+	$splitter = explode('\\', $mWindowsSplit[1]);
+	$valueName = $mWindowsSplit[2] ?? end($splitter);
+
+	if(startsWith($mWindowsSplit[1], 'Software\Policies\Google\Chrome')) {
+		$mLinux = 'JSON:/etc/opt/chrome/policies/managed/default.json:'.$valueName."\n"
+			.'JSON:/etc/chromium/policies/managed/default.json:'.$valueName;
+		$mMacOs = 'DEFAULTS:/Library/Preferences/com.google.Chrome:'.$valueName;
+
+	} elseif(startsWith($mWindowsSplit[1], 'Software\Policies\Google\Chrome\Recommended')) {
+		$mLinux = 'JSON:/etc/opt/chrome/policies/recommended/default.json:'.$valueName."\n"
+			.'JSON:/etc/chromium/policies/recommended/default.json:'.$valueName;
+		$mMacOs = null;
+
 	} elseif(startsWith($mWindowsSplit[1], 'Software\Policies\Mozilla\Firefox')) {
 		$prefix = substr($mWindowsSplit[1], 33, 1) == '\\' ? substr($mWindowsSplit[1], 34) : ''; // cut first backslash!
 		if(!empty($prefix)) $prefix .= '\\';
 		$prefix = 'policies\\'.$prefix;
-		$mLinux = 'JSON:/etc/firefox/policies/policies.json:'.$prefix.$mWindowsSplit[2];
+		$mLinux = trim('JSON:/etc/firefox/policies/policies.json:'.$prefix.($mWindowsSplit[2] ?? ''), '\\');
+		$mMacOs = null;
+
 	} else {
+		// neither a Chrome or Firefox policy
 		continue;
 	}
 
-	echo 'Set Linux manifestation: '.$mWindowsSplit[2]."\n";
+	echo 'Set Linux/macOS manifestation: #'.$pd->id.' '.$pd->name."\n";
 	$stmt = $db->getDbHandle()->prepare(
-		'UPDATE policy_definition SET manifestation_linux = :manifestation_linux WHERE id = :id'
+		'UPDATE policy_definition SET manifestation_linux=:manifestation_linux, manifestation_macos=:manifestation_macos WHERE id = :id'
 	);
-	$stmt->execute([':id' => $pd->id, ':manifestation_linux' => $mLinux]);
+	$stmt->execute([':id' => $pd->id, ':manifestation_linux' => $mLinux, ':manifestation_macos' => $mMacOs]);
 }
