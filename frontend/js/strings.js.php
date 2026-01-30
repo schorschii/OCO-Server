2baed119bf5493ac3f3ceb257ed2aedfeaaa5005
Georg Sieber <it@georg-sieber.de>
2026-01-30 23:24:29 +0100

    simplify JS language constant generation

--- frontend/js/strings.js.php
+++ frontend/js/strings.js.php
@@ -1,9 +1,5 @@
 <?php
 require_once('../../loader.inc.php');
 header('Content-Type: text/javascript');
-
-echo "var LANG = {\n";
-foreach(LanguageController::getSingleton()->getMessages() as $key => $message) {
-    echo "\t\"".$key."\": \"".str_replace('"',"'",str_replace("\n",'\\n',str_replace('\\','\\\\',$message)))."\",\n";
-}
-echo "}\n";
+?>
+var LANG = <?php echo json_encode(LanguageController::getSingleton()->getMessages()); ?>
