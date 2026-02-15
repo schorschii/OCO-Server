6cbd4cbced1417e2218abbb6439b75fcd19c4ca2
Georg Sieber <it@georg-sieber.de>
2026-02-15 19:50:10 +0100

    fix self-service

--- frontend/views/partial/copyright.php
+++ frontend/views/partial/copyright.php
@@ -1,6 +1,5 @@
 <?php
 $SUBVIEW = 1;
 require_once(__DIR__.'/../../../loader.inc.php');
-require_once(__DIR__.'/../../session.inc.php');
 ?>
 © <a href="https://sieber.systems" target="_blank">Georg Sieber</a> 2020-2025 | <a href="https://oco.sieber.systems" target="_blank"><?php echo LANG('website'); ?></a> | <a href="https://github.com/schorschii/oco-server" target="_blank"><?php echo LANG('github'); ?></a>
