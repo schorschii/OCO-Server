e6ea5e47cf2d3f6a6b970f90eaaead0f9bcfd13d
Georg Sieber <it@georg-sieber.de>
2026-02-17 09:25:47 +0100

    handle generic exception

--- frontend/views/dialog/profile-details.php
+++ frontend/views/dialog/profile-details.php
@@ -12,7 +12,7 @@ try {
 	} catch(DOMException|TypeError $e) {
 		$payloadData = json_decode($profile->payload, true);
 		if($payloadData === null)
-			throw new InvalidRequestException('Payload is no valid XML or JSON');
+			throw new Exception('Payload is no valid XML or JSON');
 	}
 } catch(PermissionException $e) {
 	http_response_code(403);
@@ -22,6 +22,9 @@ try {
 	die(LANG('permission_denied'));
 } catch(InvalidRequestException $e) {
 	http_response_code(400);
+	die($e->getMessage());
+} catch(Exception $e) {
+	http_response_code(500);
 	die($e->getMessage());
 }
 
