2082d9f4b258a14cafabc636863525a9763eb239
Georg Sieber <it@georg-sieber.de>
2026-02-18 21:03:27 +0100

    update year

--- frontend/views/partial/copyright.php
+++ frontend/views/partial/copyright.php
@@ -2,4 +2,4 @@
 $SUBVIEW = 1;
 require_once(__DIR__.'/../../../loader.inc.php');
 ?>
-© <a href="https://sieber.systems" target="_blank">Georg Sieber</a> 2020-2025 | <a href="https://oco.sieber.systems" target="_blank"><?php echo LANG('website'); ?></a> | <a href="https://github.com/schorschii/oco-server" target="_blank"><?php echo LANG('github'); ?></a>
+© <a href="https://sieber.systems" target="_blank">Georg Sieber</a> 2020-2026 | <a href="https://oco.sieber.systems" target="_blank"><?php echo LANG('website'); ?></a> | <a href="https://github.com/schorschii/oco-server" target="_blank"><?php echo LANG('github'); ?></a>
