d2d24d8ee15f840d2839aa962b3779dd96b552c8
Georg Sieber <it@georg-sieber.de>
2026-08-05 15:51:46 +0200

    apply new extension behavior to js, css, img too

--- frontend/views/index.php
+++ frontend/views/index.php
@@ -3,8 +3,7 @@ $SUBVIEW = 1;
 require_once('../../loader.inc.php');
 require_once('../session.inc.php');
 
-// this script is called via Apache RewriteRule if the requested filename was not found in this directory
-// in this case, we check if there is an extension available which can handle this request
+// this script is called via Apache RewriteRule for all requests of this directory
 
 $requestUrl = explode('?', $_SERVER['REQUEST_URI']);
 $requestUrlPath = reset($requestUrl);
