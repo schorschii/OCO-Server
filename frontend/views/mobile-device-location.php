a08bb3dffc890c8b089b97b40e2a4f486ca85557
Georg Sieber <it@georg-sieber.de>
2026-08-17 14:47:17 +0200

    improve layout

--- frontend/views/mobile-device-location.php
+++ frontend/views/mobile-device-location.php
@@ -29,63 +29,64 @@ $locationButtonDisabled = $isSupervised !== true
 	|| $lostModeCommandPending;
 ?>
 
-<div class='controls heading'>
-	<h2><?php echo LANG('location'); ?></h2>
-	<div class='filler invisible'></div>
-	<button onclick='requestMobileDeviceLocation(this, <?php echo intval($md->id); ?>);return false' <?php if($locationButtonDisabled) echo 'disabled'; ?>>
-		<img src='img/refresh.dyn.svg'>&nbsp;<?php echo LANG('location_refresh'); ?>
-	</button>
-</div>
+<div class='details-abreast'>
+	<div>
+		<div class='controls heading'>
+			<h2><?php echo LANG('data'); ?></h2>
+			<div class='filler invisible'></div>
+			<?php if($openStreetMapUrl !== null) { ?>
+				<button onclick='window.open(this.getAttribute("data-url"), "_blank", "noopener,noreferrer");return false'
+					data-url='<?php echo htmlspecialchars($openStreetMapUrl, ENT_QUOTES); ?>'>
+					<img src='img/eye.dyn.svg'>&nbsp;<?php echo LANG('open_in_openstreetmap'); ?>
+				</button>
+			<?php } ?>
+			<button onclick='requestMobileDeviceLocation(this, <?php echo intval($md->id); ?>);return false' <?php if($locationButtonDisabled) echo 'disabled'; ?>>
+				<img src='img/refresh.dyn.svg'>&nbsp;<?php echo LANG('location_refresh'); ?>
+			</button>
+		</div>
 
-<?php if($deviceLocation !== null) { ?>
-<table class='list metadata'>
-	<tr>
-		<th><?php echo LANG('last_located'); ?></th>
-		<td><?php echo htmlspecialchars(date('Y-m-d H:i:s', strtotime($deviceLocation['Timestamp']??''))); ?></td>
-	</tr>
-	<tr>
-		<th><?php echo LANG('latitude'); ?></th>
-		<td><?php echo htmlspecialchars(number_format($deviceLocation['Latitude'], 6, '.', '')); ?></td>
-	</tr>
-	<tr>
-		<th><?php echo LANG('longitude'); ?></th>
-		<td><?php echo htmlspecialchars(number_format($deviceLocation['Longitude'], 6, '.', '')); ?></td>
-	</tr>
-	<tr>
-		<th><?php echo LANG('accuracy'); ?></th>
-		<td><?php
-			$horizontalAccuracy = MobileDeviceLocationEvaluator::getHorizontalAccuracy($deviceLocation);
-			echo $horizontalAccuracy !== null
-				? htmlspecialchars(round($horizontalAccuracy, 1).' m')
-				: '-';
-		?></td>
-	</tr>
-</table>
-<?php } ?>
+		<?php if($isSupervised === false) { ?>
+			<div class='alert warning'><?php echo LANG('device_location_requires_supervision'); ?></div>
+		<?php } elseif($isSupervised === null) { ?>
+			<div class='alert warning'><?php echo LANG('device_location_supervision_unknown'); ?></div>
+		<?php } elseif($lostModeCommandPending) { ?>
+			<div class='alert warning'><?php echo LANG('device_location_lost_mode_pending'); ?></div>
+		<?php } elseif($isLostModeEnabled === false) { ?>
+			<div class='alert warning'><?php echo LANG('device_location_requires_lost_mode'); ?></div>
+		<?php } elseif($isLostModeEnabled === null) { ?>
+			<div class='alert warning'><?php echo LANG('device_location_lost_mode_unknown'); ?></div>
+		<?php } elseif($locationRequestPending) { ?>
+			<div class='alert info'><?php echo LANG('device_location_pending'); ?></div>
+		<?php } elseif($locationRequestFailed) { ?>
+			<div class='alert warning'><?php echo LANG('device_location_failed'); ?></div>
+		<?php } elseif($deviceLocation === null) { ?>
+			<div class='alert info'><?php echo LANG('device_location_none'); ?></div>
+		<?php } ?>
 
-<?php if($isSupervised === false) { ?>
-	<div class='alert warning'><?php echo LANG('device_location_requires_supervision'); ?></div>
-<?php } elseif($isSupervised === null) { ?>
-	<div class='alert warning'><?php echo LANG('device_location_supervision_unknown'); ?></div>
-<?php } elseif($lostModeCommandPending) { ?>
-	<div class='alert warning'><?php echo LANG('device_location_lost_mode_pending'); ?></div>
-<?php } elseif($isLostModeEnabled === false) { ?>
-	<div class='alert warning'><?php echo LANG('device_location_requires_lost_mode'); ?></div>
-<?php } elseif($isLostModeEnabled === null) { ?>
-	<div class='alert warning'><?php echo LANG('device_location_lost_mode_unknown'); ?></div>
-<?php } elseif($locationRequestPending) { ?>
-	<div class='alert info'><?php echo LANG('device_location_pending'); ?></div>
-<?php } elseif($locationRequestFailed) { ?>
-	<div class='alert warning'><?php echo LANG('device_location_failed'); ?></div>
-<?php } elseif($deviceLocation === null) { ?>
-	<div class='alert info'><?php echo LANG('device_location_none'); ?></div>
-<?php } ?>
-
-<?php if($openStreetMapUrl !== null) { ?>
-	<div class='controls'>
-		<button onclick='window.open(this.getAttribute("data-url"), "_blank", "noopener,noreferrer");return false'
-			data-url='<?php echo htmlspecialchars($openStreetMapUrl, ENT_QUOTES); ?>'>
-			<img src='img/eye.dyn.svg'>&nbsp;<?php echo LANG('open_in_openstreetmap'); ?>
-		</button>
+		<?php if($deviceLocation !== null) { ?>
+		<table class='list metadata'>
+			<tr>
+				<th><?php echo LANG('last_located'); ?></th>
+				<td><?php echo htmlspecialchars(date('Y-m-d H:i:s', strtotime($deviceLocation['Timestamp']??''))); ?></td>
+			</tr>
+			<tr>
+				<th><?php echo LANG('latitude'); ?></th>
+				<td><?php echo htmlspecialchars(number_format($deviceLocation['Latitude'], 6, '.', '')); ?></td>
+			</tr>
+			<tr>
+				<th><?php echo LANG('longitude'); ?></th>
+				<td><?php echo htmlspecialchars(number_format($deviceLocation['Longitude'], 6, '.', '')); ?></td>
+			</tr>
+			<tr>
+				<th><?php echo LANG('accuracy'); ?></th>
+				<td><?php
+					$horizontalAccuracy = MobileDeviceLocationEvaluator::getHorizontalAccuracy($deviceLocation);
+					echo $horizontalAccuracy !== null
+						? htmlspecialchars(round($horizontalAccuracy, 1).' m')
+						: '-';
+				?></td>
+			</tr>
+		</table>
+		<?php } ?>
 	</div>
-<?php } ?>
+</div>
