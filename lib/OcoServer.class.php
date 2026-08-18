db5bc8684128e89f42d2efcb0c8d1947ebe69bc6
Georg Sieber <it@georg-sieber.de>
2026-08-18 10:39:04 +0200

    bump version

--- lib/OcoServer.class.php
+++ lib/OcoServer.class.php
@@ -2,8 +2,8 @@
 
 class OcoServer {
 
-	const APP_VERSION = '1.2.1';
-	const APP_RELEASE = '"Electric Horse"';
+	const APP_VERSION = '1.2.2';
+	const APP_RELEASE = '"Viral Voting"';
 
 	const UPDATE_API_URL = 'https://api.github.com/repos/schorschii/oco-server/releases';
 
