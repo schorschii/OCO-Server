1a9d1173a2ec2efadee9409383d55d4e6d621f5d
Georg Sieber <Georg.Sieber@slub-dresden.de>
2026-09-22 13:27:52 +0200

    fix about dialog in self-service

--- self-service/views/dialog/about.php
+++ self-service/views/dialog/about.php
@@ -1,1 +1,6 @@
-../../../frontend/views/dialog/about.php
+<?php
+$SUBVIEW = 1;
+require_once(__DIR__.'/../../../loader.inc.php');
+require_once(__DIR__.'/../../session.inc.php');
+
+require(__DIR__.'/../partial/about.php');
